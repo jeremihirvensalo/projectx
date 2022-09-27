d49dff1eeb2fb555a74c4957d8097e9097dd56be
JeremiH04 <jeremi.hirvensalo@gmail.com>
2022-09-27 13:41:14 +0300

    /player logic made

--- projectx/main/gameServer.js
+++ projectx/main/gameServer.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Database = require("./pages/database/db");
 const app = express();
 const config = require("./config.json")[0].gameServer;
 const Database = require("./pages/database/db.js");
@@ -11,20 +10,24 @@ app.post("/player", async (req, res)=>{
     const player = req.body;
     if(!player.token) res.json({status:401});
     if(!player.x || !player.y || !player.w || !player.h || !player.name) res.json({info:false});
+    try{
+        const user = await db.searchUser(player.name);
+        if(user.username){
+            const userToken = await db.searchToken(user.username);
+            if(!userToken.err){
+                if(db.compareTokens(player.token, userToken.token)){
+                    // Tässä pelaajan lisäys tietokantaan
 
-    const user = await db.searchUser(player.name);
-    if(!user.info && !user.err){
-        const userToken = await db.searchToken(user.username);
-        if(!userToken.err){
-            if(db.compareTokens(player.token, userToken)){
-                // Tässä pelaajan lisäys tietokantaan
-
-                res.json({info:true});
-            }
-        } 
+                    res.json({info:true});
+                }
+            } 
+        }else res.json({info:false});
+        
+    }catch(e){
+        console.log(e.message);
         res.json({info:false});
     }
-    res.json({info:false});
+
 });
 
 app.listen(config.port, ()=>console.log("Peliservu liekeis"));
